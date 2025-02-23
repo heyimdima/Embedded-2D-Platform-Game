cebe090dd15139fc57bc42ddfbcf4b04ea04f139
Dima Bondar <59861277+heyimdima@users.noreply.github.com>
2025-02-23 09:51:37 -0700

    Update game.h

--- game.h
+++ game.h
@@ -1,6 +1,8 @@
 // SWE-450 Milestone 3 (Doodle Jump Game)
 // Dima Bondar
 // 02/23/2025
+// Reference: A lot of the ideas of score tracking/game loop/platform generation come from my Python Doodle Jump clone I did 4 years ago.
+// Link: https://github.com/heyimdima/FoxJumpGame
 
 #ifndef BLACKJACK_H
 #define BLACKJACK_H
